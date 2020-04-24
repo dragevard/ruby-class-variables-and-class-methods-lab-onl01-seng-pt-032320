2fa8213f23d7fd57a23cbb97398368f12c322fd6
Derek phung <dragevards@gmail.com>
2020-04-24 16:37:31 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = [] 
+  @@artists = []
+  
+  def initialize(song, artist, genre)
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    @name = song 
+    @artist = artist 
+    @genre = genre 
+  end 
+  
+  def self.count
+    @@count 
+  end 
+  
+  def self.genres
+    @@genres.uniq 
+  end 
+    
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    new_hash = Hash.new(0)
+    @@genres.each do |x|
+      new_hash[x] += 1 
+    end 
+    new_hash
+  end
+  
+  def self.artist_count
+    new_hash = Hash.new(0)
+    @@artists.each do |x|
+      new_hash[x] += 1 
+    end 
+    new_hash
+  end 
+  
+end
