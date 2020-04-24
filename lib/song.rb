7f10518c32b3f73312a1e63e2d92f0a1c4d2b648
Derek phung <dragevards@gmail.com>
2020-04-24 16:23:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,11 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
+  @@genres = [] 
   
-  def initialize(song)
+  def initialize(song, genre)
     @@count += 1
+    @@genres << genre
   end 
   
   def self.count
@@ -13,5 +15,5 @@ class Song
   end 
   
   def self.genres
-    
+    @@genres.uniq 
 end
